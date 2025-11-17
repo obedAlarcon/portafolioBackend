bc480c9fca5347c8792eebaf867464e51819dcdf
obedAlarcon <desarrolloc20@hotmail.com>
2025-11-17 14:21:08 -0500

    archivos config

--- config/config.js
+++ config/config.js
@@ -1,26 +1,41 @@
-require ('dotenv').config();
-
-const config = {
- 
-
-
-env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 5000,
-  apiKey: process.env.API_KEY,
-  jwtSecret: process.env.JWT_SECRET,
-  smtpEmail: process.env.SMTP_EMAIL,
-  smtpPassword: process.env.SMTP_PASSWORD,
-  
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbPort: process.env.DB_PORT,
-  dbName: process.env.DB_NAME,
-  
-  databaseUrl: process.env.DATABASE_URL,
-
-}
-module.exports={config}
+const express = require('express')
+const AuthService = require('../services/auth.service')
+const { error } = require('console')
+const {config}=require('../config/config')
+const service = new AuthService()
+const router=express.Router()
 
 
 
+router.post('/',async (req, res, next)=>{
+    try {
+        const {nombre,  email, mensaje}=req.body
+        if(!nombre || !email || !mensaje){
+            return res.status(400).json({
+                error:'Todos los campos son obligatorios'
+            })
+        }
+            const mail = {
+               from: `"Contacto desde el portafolio" <${config.smtpEmail}>`, // siempre tu cuenta validada
+  to: config.smtpEmail, // a ti mismo (tu buzón)
+  subject: 'Nuevo mensaje desde tu portafolio',
+  text: `Has recibido un mensaje de ${nombre} (${email}): ${mensaje}`,
+  html: `
+    <h3>Has recibido un nuevo mensaje</h3>
+    <p><strong>Nombre:</strong> ${nombre}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Mensaje:</strong></p>
+    <p>${mensaje}</p>
+  `,
+  replyTo: email // aquí sí pones el correo del cliente
+              };
+         const result= await service.sendMail(mail)
+         res.json({success:true, result})
+        
+    } catch (error) {
+        
+        next(error)
+    }
+})
+
+module.exports = router;
